a882e76357f85af9613c78ec309087f61d5b634c
shreeja <shreeja@excellisit.net>
2024-07-02 11:33:06 -0700

    many changes

--- resources/views/feeds/filter.blade.php
+++ resources/views/feeds/filter.blade.php
@@ -1,58 +1,30 @@
 @if (count($feeds) > 0)
-<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-5">
     @foreach ($feeds as $feed)
-        <div class="col mb-4">
-            <div class="food-box">
-                
-                <div class="food-status">
-                    <div class="food-status-1">
-                        <h4>Title:</h4>
-                    </div>
-                    <div class="food-status-2">
-                        <h4>{{ $feed->title ?? 'N/A' }}</h4>
-                    </div>
-                </div>
+        <tr>
+            <td class="edit-route" data-route="{{ route('members.edit', $feed['id']) }}">
+                {{ $feed->title }}
+            </td>
 
-                <div class="food-status">
-                    <div class="food-status-1">
-                        <h4>Content:</h4>
-                    </div>
-                    <div class="food-status-2 company_address">
-                        <h4>{{ Str::limit($feed->content) ?? 'N/A' }}</h4>
-                    </div>
-                </div>
-
-                <div class="food-status">
-                    <div class="food-status-1">
-                        <h4>File Upload</h4>
-                    </div>
-                    <div class="food-status-2">
-                        <h4>{{ $feed->created_at ?? 'N/A' }}</h4>
-                    </div>
-                </div>
-                
-                <div class="">
-                    <a href="{{route('feeds.edit', Crypt::encrypt($feed->id))}}" class="btn-2">See Open Feeds</a>
-                </div>
-            </div>
-        </div>
+            <td >
+                {{ $feed->created_at->format('d-m-Y') }}
+            </td>
+            
+            
+        </tr>
     @endforeach
-    {{-- pagination --}}
-</div>
-<div class="row">
-    <div class="col-xl-12">
-        <div class="d-flex justify-content-center">
-            {{ $feeds->links() }}
-        </div>
-    </div>
-</div>
+    <tr class="toxic">
+        <td colspan="6" class="text-left">
+            <div class="d-flex justify-content-between">
+                <div class="">
+                     (Showing {{ $feeds->firstItem() }} – {{ $feeds->lastItem() }} feeds of
+                    {{$feeds->total() }} feeds)
+                </div>
+                <div>{!! $feeds->links() !!}</div>
+            </div>
+        </td>
+    </tr>
 @else
-{{-- no data found --}}
-<div class="row">
-    <div class="col-xl-12">
-        <div class="d-flex justify-content-center">
-            <h3>No Data Found</h3>
-        </div>
-    </div>
-</div>
+    <tr>
+        <td colspan="6" class="text-center">No Data Found</td>
+    </tr>
 @endif
